b0aaaa8d64ae7aa6b3164de19dbf88a11397c5d9
Dawid Marszałkiewicz <dms@spyro-soft.com>
2025-07-14 11:47:54 +0000

    feat: add logging

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,19 +1,14 @@
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
 
 #define SLEEP_TIME_MS 1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-
 int main(void)
 {
-    if (!gpio_is_ready_dt(&led)) return 0;
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
-
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
-
+        LOG_INF("Hello from Zephyr");
         k_msleep(SLEEP_TIME_MS);
     }
 }
