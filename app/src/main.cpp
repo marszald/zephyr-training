ee782dc4e08946d4ed004b651c7a0b48cc33debb
Dawid Marszałkiewicz <dawid.marszalkiewicz@goodbyte.pl>
2025-07-14 14:57:32 +0200

    feat: add fluke_board

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,5 +1,10 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
+
+/* The devicetree node identifier for the "led1" alias. */
+#define LED_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main);
 
@@ -7,7 +12,12 @@ LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
+    if (!gpio_is_ready_dt(&led)) return 0;
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+
     while (1) {
+        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+
         LOG_INF("Hello from Zephyr");
         k_msleep(SLEEP_TIME_MS);
     }
